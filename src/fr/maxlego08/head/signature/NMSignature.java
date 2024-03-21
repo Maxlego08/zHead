ae9c63dd3f02c168253b009c95f4f9f0e59abf45
Maxlego08 <maxlego08@sfr.fr>
2024-03-21 18:54:38 +0100

    :memo: Add nms signature

--- src/fr/maxlego08/head/signature/NMSignature.java
+++ src/fr/maxlego08/head/signature/NMSignature.java
@@ -2,6 +2,7 @@ package fr.maxlego08.head.signature;
 
 import fr.maxlego08.head.api.Head;
 import fr.maxlego08.head.api.HeadSignature;
+import fr.maxlego08.head.zcore.utils.nms.ItemStackCompound;
 import org.bukkit.inventory.ItemStack;
 
 public class NMSignature implements HeadSignature {
@@ -9,11 +10,11 @@ public class NMSignature implements HeadSignature {
     @Override
     public ItemStack sign(ItemStack itemStack, Head head) {
         if (itemStack == null) return null;
-        return itemStack;
+        return ItemStackCompound.itemStackCompound.setString(itemStack, HeadSignature.KEY, head.getId());
     }
 
     @Override
     public String getHeadId(ItemStack itemStack) {
-        return "TODO";
+        return ItemStackCompound.itemStackCompound.getString(itemStack, HeadSignature.KEY);
     }
 }
